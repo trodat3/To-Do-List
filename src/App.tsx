7fd83a298d3d08d0dfc4b7347c64be8f71bcbee2
trodat3 <joaopedroabrantescarvalho@gmail.com>
2025-09-03 09:42:13 -0300

    Modificações iniciais, definindo esqueleto

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  
+  const[inputNomeTarefa, setValorNomeTarefa] = useState("");
+
+  function handleCriar(){
+    if (inputNomeTarefa){
+        setValorNomeTarefa(inputNomeTarefa);
+    }
+  }
+
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen text-center font-sans'>
+      <h1>ToDo List</h1>
+      
+      <div>
+        <input 
+          type="text" 
+          placeholder="Ei, digite aqui sua tarefa" 
+          value={inputNomeTarefa}
+          onChange={(evento) => setValorNomeTarefa(evento.target.value)}
+          className="input-tarefa"
+        />
+        
+        <button onClick={handleCriar} style={{ padding: '8px' }}>
+          Criar
+        </button>
+      </div>
+      </div>
+
+  );
 }
 
 export default App
